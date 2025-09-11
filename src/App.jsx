a386ce700d54bbc52e7ebed03ea1a7cc720216fd
LautaroCasalini <lcasalini@uade.edu.ar>
2025-09-10 22:09:27 -0300

    1.0.0

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import MessageTable from "./components/MessageTable";
+
+export default function App() {
+  return (
+    <div className="min-h-screen min-w-screen bg-gray-100 flex justify-center items-center">
+      <main>
+        <MessageTable />
+      </main>
+    </div>
+  );
+}
+
